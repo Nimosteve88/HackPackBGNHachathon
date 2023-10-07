ecfa636a408c0ced92a1c62e29b5584fb8fcc232
Nimosteve88 <85235717+Nimosteve88@users.noreply.github.com>
2023-10-07 17:59:45 +0100

    update

--- conversationAI/conversationAIProject/members/urls.py
+++ conversationAI/conversationAIProject/members/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.members, name='members'),
+    path('', views.homepage, name='homepage'),
+    path('chatbot/', views.chatbot, name='chatbot'),
+    path('login/',views.login, name='login'),
+    path('pronouncequiz/',views.pronouncequiz, name='pronounce')
+
 ]
--- conversationAI/conversationAIProject/members/views.py
+++ conversationAI/conversationAIProject/members/views.py
@@ -4,3 +4,19 @@ from django.template import loader
 def members(request):
   template = loader.get_template('Pronouncequiz.html')
   return HttpResponse(template.render())
+
+def homepage(request):
+  template = loader.get_template('homepage.html')
+  return HttpResponse(template.render())
+
+def chatbot(request):
+  template = loader.get_template('chatbox.html')
+  return HttpResponse(template.render())
+
+def login(request):
+  template = loader.get_template('login.html')
+  return HttpResponse(template.render())
+
+def pronouncequiz(request):
+  template = loader.get_template('Pronouncequiz.html')
+  return HttpResponse(template.render())
