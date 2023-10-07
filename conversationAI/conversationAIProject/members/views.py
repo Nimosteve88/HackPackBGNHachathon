ee050c0c26968642c9d1e2112ae320624b9bf7f0
Nimosteve88 <85235717+Nimosteve88@users.noreply.github.com>
2023-10-07 17:35:35 +0100

    update

--- conversationAI/conversationAIProject/members/views.py
+++ conversationAI/conversationAIProject/members/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.template import loader
 
 def members(request):
-  template = loader.get_template('homepage.html')
+  template = loader.get_template('Pronouncequiz.html')
   return HttpResponse(template.render())
