83b442c9b92112fcd15f440fc954e0a34b79bb27
Nimosteve88 <85235717+Nimosteve88@users.noreply.github.com>
2023-10-07 10:29:20 +0100

    Uppdate

--- conversationAI/conversationAIProject/conversationAIProject/settings.py
+++ conversationAI/conversationAIProject/conversationAIProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@9n0ug-a(4l0u4e05sgf@y$6c(mzb+d-&p4221z=1wsck9o=it'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'members'
 ]
 
 MIDDLEWARE = [
