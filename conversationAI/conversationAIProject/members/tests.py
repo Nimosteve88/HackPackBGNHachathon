a21b807dc702176e6335fb92d55acc5a48590025
Nimosteve88 <85235717+Nimosteve88@users.noreply.github.com>
2023-10-07 17:06:38 +0100

    update

--- conversationAI/conversationAIProject/members/tests.py
+++ conversationAI/conversationAIProject/members/tests.py
@@ -1,18 +1,38 @@
-from django.test import TestCase
-import os
-import openai
-print("hello")
-openai.api_key = "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"
+from google.cloud import dialogflow 
+from google.cloud import pubsub
+import json
 
-response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-        {"role": "system", "content": "You are a helpful AI Tutor."},
-        {"role": "user", "content": "I am Pradip, I want to learn AI"},
-        {"role": "assistant", "content": "Hello, Pradip Thats awesome, what do you want to know aboout AI"},
-        {"role": "user", "content": "What is NLP?"}
-    ]
-)
+def main():
+    # Create a Dialogflow client
+    dialogflow_client = dialogflow.SessionsClient()
 
-# Print the model's response
-print(response.choices[0].text)
+    # Create a Pub/Sub client
+    pubsub_client = pubsub.Client()
+
+    # Create a Pub/Sub topic to receive chat messages from the Dialogflow CX widget
+    topic_name = "my-chat-topic"
+    topic = pubsub_client.topic(topic_name)
+
+    # Subscribe to the Pub/Sub topic
+    subscription = topic.subscribe()
+
+    # Start a loop to listen for chat messages from the Dialogflow CX widget
+    while True:
+        message = subscription.receive(timeout=10)
+        if message is not None:
+            # The message is a JSON object containing the chat message
+            chat_message = json.loads(message.data)
+
+            # Do whatever you want with the chat message, such as logging it to a database or sending it to a notification service
+
+            message.ack()
+        else:
+            # There are no new chat messages
+            break
+
+def log_chat_message(chat_message):
+    # TODO: Implement this function to log the chat message to a database
+    pass
+
+if __name__ == "__main__":
+    main()
